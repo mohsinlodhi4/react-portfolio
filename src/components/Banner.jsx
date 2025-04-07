7d77165737f1e8dd27e90374a6c1cf5d8de8dd9f
mohsin-idenfo <mohsin.lodhi@idenfo.com>
2025-04-08 04:28:57 +0500

    banner fix

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -19,11 +19,15 @@ const Banner = () => {
                             <div className="index_banner_left" data-aos="zoom-out" data-aos-duration="2000">
                                 <div className="index_banner_wrap_text">
                                     <h6>Mohsin Khan Lodhi</h6>
-                                    <p>A passionate Software Engineer dedicated to crafting innovative and efficient web applications. With a strong background in full-stack development, I specialize in building scalable, high-performance solutions that solve real-world problems.</p>
+                                    <p>Hi, I am a Software Engineer with focus on Software Architecture and AI integration. With a strong background in full-stack development, I specialize in building scalable, high-performance solutions that solve real-world problems.</p>
                                     {/* <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae neque corrupti totam beatae sed voluptatibus saepe ex ullam voluptatem. Atque officiis ratione molestiae enim sapiente commodi! Repellat blanditiis recusandae velit hic tempora accusantium voluptatem vero!</p> */}
                                 </div>
                                 <div className="index_banner_btn">
-                                    <a href="#footer_form" className="common_btn" >Let's Chat</a>
+                                    <div className="index_banner_btn_inner">
+                                        <a href="https://www.linkedin.com/in/mohsin-lodhi-53685b202/" target='_blank' className="common_btn" >LinkedIn <img width={35} src="/images/linkedin.png"/> </a>
+                                        &nbsp;&nbsp;
+                                        <a href="https://github.com/mohsinlodhi4" target='_blank' className="common_btn" >Github &nbsp;&nbsp; <img width={30} src="/images/github.png"/> </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
