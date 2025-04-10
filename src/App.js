c643c5984346ee17d24f48ca4d5cbc3cfa67071a
mohsin-idenfo <mohsin.lodhi@idenfo.com>
2025-04-10 05:15:09 +0500

    route change

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path='/' element={<Main/>} />
-      <Route path='/gallery' element={<Gallery/>} />
+      <Route path='*' element={<Main/>} />
+      {/* <Route path='/gallery' element={<Gallery/>} /> */}
     </Routes>
     </>
   );
